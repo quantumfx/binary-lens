2adc992d66243c64006c8899de0c21e9a55694ec
Fang Xi Lin <flin@cita.utoronto.ca>
2017-11-06 11:30:02 -0500

    current

--- job_scinet.sh
+++ job_scinet.sh
@@ -10,4 +10,4 @@ cd $PBS_O_WORKDIR
  
 # EXECUTION COMMAND; -np = nodes*ppn
 # Frequency band is 311.25MHz+16MHz*NFREQ to 311.25MHZ+2*16MHz*NFREQ, where NFREQ is the number after "lensing.py"
-mpirun -np 240 python "lensing.py" 0 #1> out.txt 2> error.txt
+mpirun -np 240 python "lensing_07.py" 2 #1> out.txt 2> error.txt
--- lensing_02.py
+++ lensing_02.py
@@ -24,7 +24,7 @@ fref = fmin #Hz
 intime = 32/8000 # second/point
 
 # Output prefix
-FileName = "data/constrained_01_"
+FileName = "data/constrained_0.2Hz_freqcut_"
 
 # Generates signal
 def Signal(width=PulseWidth*fsample, length=int(Period*fsample), Noise=0):
@@ -102,7 +102,7 @@ def DisPath():
 dispath = np.empty( 4*len(DisPath()) )
 if rank == 0:
     #dispath = DisPath()
-    dispath = np.load('data/dm_constrained_01.npy')
+    dispath = np.load('data/constrained_0.2Hz.npy')
 comm.Bcast(dispath, root=0)
 
 #increasing density, do this if you're generating a new dispath
