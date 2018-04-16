08305a41002ce09eaeec7128751acdc6017b5f4e
Fang Xi Lin <flin@nia-login01.scinet.local>
2018-04-16 11:11:38 -0400

    pre-sim

--- job_scinet.sh
+++ job_scinet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=30:ppn=8,walltime=2:00:00
+#PBS -l nodes=6:ppn=40,walltime=2:00:00
 #PBS -N lensing
  
 # load modules (must match modules used for compilation)
@@ -10,4 +10,4 @@ cd $PBS_O_WORKDIR
  
 # EXECUTION COMMAND; -np = nodes*ppn
 # Frequency band is 311.25MHz+16MHz*NFREQ to 311.25MHZ+16MHz*(NFREQ+1), where NFREQ is the number after "lensing.py"
-mpirun -np 240 python "lensing_negconst_B.py" -1 #1> out.txt 2> error.txt
+mpirun -np 240 python "lensing_null_B.py" -1 #1> out.txt 2> error.txt
