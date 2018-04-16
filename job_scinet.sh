00c6a5ccf64e4d656dbae1a0f708d134635cad9d
Fang Xi Lin <flin@cita.utoronto.ca>
2018-04-16 11:42:37 -0400

    niagara submission

--- job_scinet.sh
+++ job_scinet.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#PBS -l nodes=6:ppn=40,walltime=2:00:00
-#PBS -N lensing
+#SBATCH --nodes=6
+#SBATCH --ntasks=240
+#SBATCH time=2:00:00
+#SBATCH --job-name lensing
  
 # load modules (must match modules used for compilation)
-module load intel/15.0.2 intelmpi python/2.7.8
+module load intel intelmpi python/2.7.14
  
 # DIRECTORY TO RUN - $PBS_O_WORKDIR is directory job was submitted from
-cd $PBS_O_WORKDIR
+cd /scratch/p/pen/flin/lensing-sim-data/
  
 # EXECUTION COMMAND; -np = nodes*ppn
 # Frequency band is 311.25MHz+16MHz*NFREQ to 311.25MHZ+16MHz*(NFREQ+1), where NFREQ is the number after "lensing.py"
-mpirun -np 240 python "lensing_null_B.py" -1 #1> out.txt 2> error.txt
+mpirun -np 240 python "~/gpc-scratch/binary-lens-B/lensing_varying_posslow_B.py" -1 #1> out.txt 2> error.txt
