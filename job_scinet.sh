7ba32d7299c1daac7f1775e40bda8b4f89b8306b
Fang Xi Lin <flin@cita.utoronto.ca>
2018-05-16 19:54:28 -0400

    corrected errors in job script

--- job_scinet.sh
+++ job_scinet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --nodes=6
-#SBATCH --ntasks=240
+#SBATCH --nodes=4
+#SBATCH --ntasks=160
 #SBATCH --time=2:00:00
 #SBATCH --job-name lensing
  
@@ -9,8 +9,8 @@ module purge
 module load gcc/7.3.0 python/3.6.5 openmpi/3.1.0
 #module load intel intelmpi python/2.7.14
  
-# DIRECTORY TO RUN - $PBS_O_WORKDIR is directory job was submitted from
-cd /scratch/p/pen/flin/lensing-sim-data/
+# DIRECTORY TO RUN - $SLURM_SUBMIT_DIR is directory job was submitted from
+cd $SLURM_SUBMIT_DIR #/scratch/p/pen/flin/lensing-sim-data/
  
 # EXECUTION COMMAND; -np = nodes*ppn
 # Frequency band is 311.25MHz+16MHz*NFREQ to 311.25MHZ+16MHz*(NFREQ+1), where NFREQ is the number after "lensing.py"
