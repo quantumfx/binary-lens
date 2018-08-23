878f625ce329bed916494bc6c9551ae66ee258e2
Fang Xi Lin <flin@cita.utoronto.ca>
2018-08-23 17:23:56 -0400

    low frequency run run on corrugated sheets

--- job_scinet.sh
+++ job_scinet.sh
@@ -3,6 +3,7 @@
 #SBATCH --ntasks=160
 #SBATCH --time=2:00:00
 #SBATCH --job-name lensing
+#SBATCH --array 0-10
  
 # load modules (must match modules used for compilation)
 module purge
@@ -14,4 +15,4 @@ cd $SLURM_SUBMIT_DIR #/scratch/p/pen/flin/lensing-sim-data/
  
 # EXECUTION COMMAND; -np = nodes*ppn
 # Frequency band is 311.25MHz+16MHz*NFREQ to 311.25MHZ+16MHz*(NFREQ+1), where NFREQ is the number after "lensing.py"
-mpirun -np 160 python3 "~/gpc-scratch/binary-lens/lensing.py" 9 #1> out.txt 2> error.txt
+mpirun -np 160 python3 "~/gpc-scratch/binary-lens/lensing.py" $SLURM_ARRAY_TASK_ID #1> out.txt 2> error.txt
